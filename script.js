c799008356bde407f2aaa8b6df67967c0a04cf8f
Richard Clarke <freshnewrich@gmail.com>
2022-03-17 14:20:01 -0700

    weird

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ const $code = $("#code")
 // console.log($code)
 var toasty = new Audio('./wav/toasty.wav');
 const $forden = $("#forden")
-$forden.hide()
+// $forden.hide()
+// console.log($forden[0].style)
+
 
 $code.on("click", (event) => {
     //play sound
     toasty.play()
     //expose element
-    $forden.show()
+    // $forden.show()
+    $forden[0].style.visibility = "visible"
+
     //delay, hide element
     setTimeout(function() {
-        $forden.hide()
+        // $forden.hide()
+        $forden[0].style.visibility = "hidden"
     }, 250)
     })
