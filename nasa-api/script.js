ce65c0ca0a5199f902bc861ea09c8a467d4932eb
Richard Clarke <freshnewrich@gmail.com>
2022-03-23 16:33:27 -0700

    revamp begin

--- nasa-api/script.js
+++ nasa-api/script.js
@@ -5,19 +5,32 @@
 const $nasaButton = $("#nasa-button")
 $nasaButton.on("click", () => {
 $.ajax(`https://api.nasa.gov/planetary/apod?count=3&api_key=9jBnAysOQFWacuExLUatq5NmxXIgdJqdleT3ibXP`).then((data) => {
-    console.log(data)
+    // console.log(data)
     $("#nasa-photos").html("")
         data.forEach((item) => {
             //this "<div>" is a jquery thing, it will understand that it needs both opening and closing
             const $div = $("<div>")
-            //set the html in the div
-            const $h1 = $(`<p>${item.title}</p>`)
-            const $a = $(`<a href="${item.hdurl}">`)
-            const $img = $(`<img src=${item.url} alt=${item.title}/>`)
+            //create p
+            const $p = $(`<p>${item.title}</p>`)
+            const $a = $(`<a target="_blank" href="${item.hdurl}">`)
+            // const $img = $(`<img src=${item.url} alt=${item.title}/>`)
+            const $img = $("<img/>")
+            $img.attr("src", item.url)
+            $img.attr("alt", item.title)
             $a.append($img)
-            $div.append($h1, $a)
+            $div.append($p, $a)
             //appends it to the aside
             $("#nasa-photos").append($div)
         })
+}).catch((error) => {
+    console.log("b0rk:", error)
 })
 })
+
+//let height
+// let width
+// const $bigimg = $("<img/>").attr("src", item.hdurl).on('load', function() {
+//     width = this.width
+//     height = this.height
+// });
+// const $dims = $(`<p>${width} x ${height}`)
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Frontend Project
+
+**Summary**
+| Field | Detail |
+|-------|--------|
+| Project Name | Personal Site Showcase|
+| Description | This site will be an example of several technologies I am familiar with. |
+| Developers | Rich Clarke |
+| Live Website | https://aretecore.github.io |
+| Repo | https://www.github.com/aretecore/frontend-project/ |
+
+## Problem Being Solved and Target Market
+
+Problem: My personal site is not very robust. Time to change that. This will be a good example of basic skills I can show to potential employers.
+
+## User Stories
+
+List of stories users can experience on my site.
+
+- Users should be able to see the site on desktop and mobile
+- Users can read a basic bio
+- Users can see my work with HTML and CSS
+- Users can see my work in javascript and jQuery
+- Users can see various API calls
+- Users can see my YouTube presence
+
+## User Interface Mockups
+
+![Wireframe](./img/wireframe.png)
