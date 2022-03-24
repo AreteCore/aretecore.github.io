751645b83479876154b07dd6611f5925c037a65c
Richard Clarke <freshnewrich@gmail.com>
2022-03-23 22:30:51 -0700

    further refinement, iframes

--- readme.md
+++ readme.md
@@ -11,17 +11,28 @@
 
 ## Problem Being Solved and Target Market
 
-Problem: My personal site is not very robust. Time to change that. This will be a good example of basic skills I can show to potential employers.
+Problem: My personal site is not very robust. Time to change that. This will be a good example of basic skills I can show to potential employers. 
 
+On the main page, there will be a quote of the day displayed above the nav bar at the top of the screen, using the https://quotes.rest/ API.
+
+In sub sections there will be some work with other APIs, namely one I did with the NASA API and the project for the weather API with the bonus completed.
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+- jQuery
+- AJAX
 ## User Stories
 
 List of stories users can experience on my site.
+
 
 - Users should be able to see the site on desktop and mobile
 - Users can read a basic bio
 - Users can see my work with HTML and CSS
 - Users can see my work in javascript and jQuery
-- Users can see various API calls
+- Users can see my use of various API calls
 - Users can see my YouTube presence
 
 ## User Interface Mockups
--- script.js
+++ script.js
@@ -14,8 +14,45 @@ $code.on("click", (event) => {
     $forden[0].style.visibility = "visible"
 
     //delay, hide element
-    setTimeout(function() {
+    setTimeout(function () {
         // $forden.hide()
         $forden[0].style.visibility = "hidden"
     }, 250)
+})
+
+
+$(document).ready(function () {
+    console.log("loaded!")
+    $.ajax(`https://quotes.rest/qod?language=en`).then((data) => {
+        // console.log("quote", data.contents.quotes[0].quote)
+        // console.log("author", data.contents.quotes[0].author)
+        $("#quote").text(`"${data.contents.quotes[0].quote}" - ${data.contents.quotes[0].author}`)
     })
+
+});
+
+
+
+{
+    $.ajax(`https://api.nasa.gov/planetary/apod?count=3&api_key=9jBnAysOQFWacuExLUatq5NmxXIgdJqdleT3ibXP`).then((data) => {
+        // console.log(data)
+        $("#nasa-photos").html("")
+        data.forEach((item) => {
+            //this "<div>" is a jquery thing, it will understand that it needs both opening and closing
+            const $div = $("<div>")
+            //create p
+            const $p = $(`<p>${item.title}</p>`)
+            const $a = $(`<a target="_blank" href="${item.hdurl}">`)
+            // const $img = $(`<img src=${item.url} alt=${item.title}/>`)
+            const $img = $("<img/>")
+            $img.attr("src", item.url)
+            $img.attr("alt", item.title)
+            $a.append($img)
+            $div.append($p, $a)
+            //appends it to the aside
+            $("#nasa-photos").append($div)
+        })
+    }).catch((error) => {
+        console.log("b0rk:", error)
+    })
+}
